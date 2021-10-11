c4b5e43fb86444c99956c99e4d71d9abc5564ad3
술든라이언 <85924877+jungwu2503@users.noreply.github.com>
2021-10-11 14:29:33 +0900

    Add files via upload

---
+++ Spring/src/spring/di/entity/NewlecExam.java
@@ -0,0 +1,19 @@
+package spring.di.entity;
+
+public class NewlecExam implements Exam {
+
+	private int kor;
+	private int eng;
+	private int math;
+	private int com;
+	
+	public int total() {
+		
+		return kor+eng+math+com;
+	}
+
+	public float avg() {
+		return total() / 4.0f;
+	}
+
+}
